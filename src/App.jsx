3391cafa321a649cc2da869333c64641cdbe9d1d
shubham <shubhamgupta780048@gmail.com>
2024-12-11 21:34:05 +0530

    Modified App.jsx and MyProfile.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,11 @@ import MyAppointments from './pages/MyAppointments'
 import Appointment from './pages/Appointment'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
-import Myprofile from './pages/Myprofile'
+import MyProfile from './pages/Myprofile'
+
+
+
+
 
 
 const App = () => {
@@ -26,7 +30,7 @@ const App = () => {
         <Route path='/login' element={<Login />} />
         <Route path='/about' element={<About />} />
         <Route path='/contact' element={<Contact />} />
-        <Route path='/my-profile' element={<Myprofile />} />
+        <Route path='/my-profile' element={<MyProfile/>} />
         <Route path='/my-appointments' element={<MyAppointments />} />
         <Route path='/appointment/:docId' element={<Appointment />} />
       </Routes>
